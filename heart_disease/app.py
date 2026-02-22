6188ba5857ffecbd8264755b7b402a0f0d729364
RedeemerNtumy <ntumyredeemer@gmail.com>
2026-02-22 23:15:26 +0000

    Changes to backedn

--- heart_disease/app.py
+++ heart_disease/app.py
@@ -1,25 +1,28 @@
 from flask import Flask, request, jsonify
 import joblib, pandas as pd
 from flask_cors import CORS
+import os
 
-app = Flask(__name__, template_folder="templates", static_folder="assets")
+app = Flask(__name__)
 CORS(app)
+
 model = joblib.load("assets/gs_knn.joblib")
 
 feature_cols = ['age','sex','cp','trestbps','chol','fbs',
                 'restecg','thalach','exang','oldpeak',
                 'slope','ca','thal']
 
+@app.route("/ping")
+def ping():
+    return "pong"
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    features = data['features']  # list of values
-    # Convert to DataFrame
-    df = pd.DataFrame([features], columns=feature_cols)
-    
-    # Predict
+    df = pd.DataFrame([data['features']], columns=feature_cols)
     prob = model.predict_proba(df)[0][1]
     return jsonify({'probability': float(prob)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
